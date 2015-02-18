3cf275fc3de5d40d13f48c3ef9085a4ceee86617
Bachue Zhou <bachue.shu@gmail.com>
2015-02-18 15:50:26 +0000

    add a test to optimized setjmp

--- proc/longjmp-test.c
+++ proc/longjmp-test.c
@@ -12,17 +12,20 @@ int
 main(int argc, char *argv[])
 {
     int val, ret;
+    volatile int test;
     if (argc != 2 || strcmp(argv[1], "--help") == 0)
         usageErr("%s val", argv[0]);
 
     val = atoi(argv[1]);
+    test = 1;
 
     switch (ret = setjmp(env)) {
     case 0:
+        test = 2;
         f1(val);                /* Never returns... */
         break;                  /* ... but this is good form */
     default:
-        printf("We jumped back from f1(), returns %d\n", ret);
+        printf("We jumped back from f1(), returns %d, test = %d\n", ret, test);
         break;
     }
 
